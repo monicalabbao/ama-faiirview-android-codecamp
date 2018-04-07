00b9c9c46c5b7c6f67753a4ee5961fdab5084830
android-user <android-user@fv.amacclab.edu.ph>
2018-04-07 12:43:09 +0800

    prepared exercise #2

--- app/src/main/java/com/monicalabbao/codecamp/ExerciseNumbeer2Actrivity.java
+++ app/src/main/java/com/monicalabbao/codecamp/ExerciseNumbeer2Actrivity.java
@@ -1,6 +1,12 @@
 package com.monicalabbao.codecamp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class ExerciseNumbeer2Actrivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
